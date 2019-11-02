843f0b4a456a5fddd499f147266707df0516ab52
iamsuryasikharej <iamssrofficial@gmail.com>
2019-11-03 00:26:04 +0530

    first commit

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,6 @@
-var x = function () { return "response"; };
+function fun(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+fun(1, 4);
--- hello-world.ts
+++ hello-world.ts
@@ -1,1 +1,8 @@
-var x=()=>"response"
+function fun(a:number,b:number,c?:string) {
+console.log(a);
+console.log(b);
+console.log(c);
+
+    
+}
+fun(1,4)
